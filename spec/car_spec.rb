9db806895bbe3cab60c917e598dc23682d0ea7d0
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-24 13:23:44 -0300

    skiping and pending

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,27 +2,36 @@ require 'car'
 
 describe "Car" do
   describe "attributes" do
-    it "allows reading and writing for :make" do
+    xit "allows reading and writing for :make" do #x in front of it or describe will skip the example
       car = Car.new
       car.make = "Test"
       expect(car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
+      skip('Another way of skiping examples')
       car = Car.new
       car.year = 9999
       expect(car.year).to eq(9999)
     end
     it "allows reading and writing for :color" do
+      #with no argument skip
+      skip
       car = Car.new
       car.color = "foo"
       expect(car.color).to eq("foo")
     end
 
     it "allows reading for :wheels" do
+      #pendig should be a failing test, otherwise use skip
+      #The code below pending IS evaluated not so with skip.
+      pending('Not implemented yet')
       car = Car.new
-      expect(car.wheels).to eq(4)
+      expect(car.wheels).to eq(5)
     end
+
+    it "allows writing for :doors" #pendig if no block given
+  
   end
 
   describe ".colors" do
