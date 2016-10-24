54d45df24d6cf2f16eb97faa535762029ff03bd4
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-24 15:16:37 -0300

    numeric comparison

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -63,4 +63,22 @@ describe "Expectation Matchers" do
     end
   end
 
+  describe 'numeric comparison matchers' do
+
+      it 'will match less than/greater than' do
+        expect(10).to be > 9
+        expect(10).to be >= 10
+        expect(10).to be <= 10
+        expect(9).to be < 10
+      end
+
+      it 'will match numeric ranges' do
+        expect(10).to be_between(5, 10).inclusive
+        expect(10).not_to be_between(5, 10).exclusive
+        expect(10).to be_within(1).of(11)
+        expect(5..10).to cover(9)
+      end
+
+    end
+    
 end
