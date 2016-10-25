e8be1e8e5d65779bd001394f2cfb5f77dccedcd4
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-24 22:05:17 -0300

    subject

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,33 +2,34 @@ require 'car'
 
 describe "Car" do
   describe "attributes" do
-    let(:car){ Car.new }
+    subject { Car.new }
+    #let(:car){ Car.new }
     # before(:example) do
     #   @car = Car.new
     # end
     end
     xit "allows reading and writing for :make" do #x in front of it or describe will skip the example
-      car.make = "Test"
-      expect(car.make).to eq('Test')
+      subject.make = "Test"
+      expect(subject.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
       skip('Another way of skiping examples')
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      subject.year = 9999
+      expect(subject.year).to eq(9999)
     end
     it "allows reading and writing for :color" do
       #with no argument skip
       skip
-      car.color = "foo"
-      expect(car.color).to eq("foo")
+      subject.color = "foo"
+      expect(subject.color).to eq("foo")
     end
 
     it "allows reading for :wheels" do
       #pendig should be a failing test, otherwise use skip
       #The code below pending IS evaluated not so with skip.
       pending('Not implemented yet')
-      expect(car.wheels).to eq(5)
+      expect(subject.wheels).to eq(5)
     end
 
     it "allows writing for :doors" #pendig if no block given
