4eaeb0437ac32283201611601fd24b617f2c8092
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-24 21:50:19 -0300

    basic hooks

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,23 +2,24 @@ require 'car'
 
 describe "Car" do
   describe "attributes" do
+    before(:example) do
+      @car = Car.new
+    end
+    end
     xit "allows reading and writing for :make" do #x in front of it or describe will skip the example
-      car = Car.new
-      car.make = "Test"
+      @car.make = "Test"
       expect(car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
       skip('Another way of skiping examples')
-      car = Car.new
-      car.year = 9999
+      @car.year = 9999
       expect(car.year).to eq(9999)
     end
     it "allows reading and writing for :color" do
       #with no argument skip
       skip
-      car = Car.new
-      car.color = "foo"
+      @car.color = "foo"
       expect(car.color).to eq("foo")
     end
 
@@ -26,12 +27,11 @@ describe "Car" do
       #pendig should be a failing test, otherwise use skip
       #The code below pending IS evaluated not so with skip.
       pending('Not implemented yet')
-      car = Car.new
-      expect(car.wheels).to eq(5)
+      expect(@car.wheels).to eq(5)
     end
 
     it "allows writing for :doors" #pendig if no block given
-  
+
   end
 
   describe ".colors" do
--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -294,5 +294,73 @@ describe "Expectation Matchers" do
 
    end
 
+  describe 'compound expectations' do
+
+     it 'will match using: and, or, &, |' do
+       expect([1,2,3,4]).to start_with(1).and end_with(4)
+
+       expect([1,2,3,4]).to start_with(1) & include(2)
+
+       expect(10 * 10).to be_odd.or be > 50
+
+       array = ['hello', 'goodbye'].shuffle
+       expect(array.first).to eq("hello") | eq("goodbye")
+     end
+
+   end
+
+  describe 'composing matchers' do
+     # some matchers accept matchers as arguments. (new in rspec3)
+
+     it 'will match all collection elements using a matcher' do
+       array = [1,2,3]
+       expect(array).to all( be < 5 )
+     end
+
+     it 'will match by sending matchers as arguments to matchers' do
+       string = "hello"
+       expect { string = "goodbye" }.to change { string }.
+         from( match(/ll/) ).to( match(/oo/) )
+
+       hash = {:a => 1, :b => 2, :c => 3}
+       expect(hash).to include(:a => be_odd, :b => be_even, :c => be_odd)
+       expect(hash).to include(:a => be > 0, :b => be_within(2).of(4))
+     end
+
+     it 'will match using noun-phrase aliases for matchers' do
+       # These are built-in aliases that make
+       # specs read better by using noun-based
+       # phrases instead of verb-based phrases.
+
+       # valid but awkward example
+       fruits = ['apple', 'banana', 'cherry']
+       expect(fruits).to start_with( start_with('a') ) &
+         include( match(/a.a.a/) ) &
+         end_with( end_with('y') )
+
+       # improved version of the previous example
+       # "start_with" becomes "a_string_starting_with"
+       # "end_with" becomes "a_string_ending_with"
+       # "match" becomes "a_string_matching"
+       fruits = ['apple', 'banana', 'cherry']
+       expect(fruits).to start_with( a_string_starting_with('a') ) &
+         include( a_string_matching(/a.a.a/) ) &
+         end_with( a_string_ending_with('y') )
+
+
+       # valid but awkward example
+       array = [1,2,3,4]
+       expect(array).to start_with( be <= 2 ) |
+         end_with( be_within(1).of(5) )
+
+       # improved version of the previous example
+       # "be <= 2" becomes "a_value <= 2"
+       # "be_within" becomes "a_value_within"
+       array = [1,2,3,4]
+       expect(array).to start_with( a_value <= 2 ) |
+         end_with( a_value_within(1).of(5) )
+     end
+
+   end
 
 end
