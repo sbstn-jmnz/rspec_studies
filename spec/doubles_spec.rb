2db8f1fb2b913c34b20035e2c2ba197af34157a8
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-26 12:01:30 -0300

    with message expectations

--- spec/doubles_spec.rb
+++ spec/doubles_spec.rb
@@ -116,5 +116,36 @@ describe 'Doubles' do
 
   end
 
+  context 'with message expectations' do
+
+    it "expects a call and allows a response" do
+      dbl = double("Chant")
+      expect(dbl).to receive(:hey!).and_return("Ho!")
+      dbl.hey!
+    end
+
+    it "does not matter which order" do
+      dbl = double("Multi-step Process")
+
+      expect(dbl).to receive(:step_1)
+      expect(dbl).to receive(:step_2)
+
+      dbl.step_2
+      dbl.step_1
+    end
+
+    it "works with #ordered when order matters" do
+      dbl = double("Multi-step Process")
+
+      expect(dbl).to receive(:step_1).ordered
+      expect(dbl).to receive(:step_2).ordered
+
+      dbl.step_1
+      dbl.step_2
+    end
+
+  end
+
+
 
 end
