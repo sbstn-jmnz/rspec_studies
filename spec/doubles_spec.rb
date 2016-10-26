88e989d440f271881f39b1d9ebe959933f6cd607
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-26 11:54:57 -0300

    partial test dubles

--- spec/doubles_spec.rb
+++ spec/doubles_spec.rb
@@ -46,5 +46,75 @@ describe 'Doubles' do
     expect(die.roll).to eq(6)
     expect(die.roll).to eq(6)  # continues returning last value
   end
-  
+
+  context 'with partial test doubles' do
+
+    it "allows stubbing instance methods on Ruby classes" do
+      time = Time.new(2010, 1, 1, 12, 0, 0)
+      allow(time).to receive(:year).and_return(1975)
+
+      expect(time.to_s).to eq('2010-01-01 12:00:00 -0300')
+      expect(time.year).to eq(1975)
+    end
+
+    it "allows stubbing instance methods on custom classes" do
+      class SuperHero
+        attr_accessor :name
+      end
+
+      hero = SuperHero.new
+      hero.name = 'Superman'
+    	expect(hero.name).to eq('Superman')
+
+      allow(hero).to receive(:name).and_return('Clark Kent')
+
+    	expect(hero.name).to eq('Clark Kent')
+    end
+
+    it "allows stubbing class methods on Ruby classes" do
+      fixed = Time.new(2010, 1, 1, 12, 0, 0)
+      allow(Time).to receive(:now).and_return(fixed)
+
+      expect(Time.now).to eq(fixed)
+      expect(Time.now.to_s).to eq('2010-01-01 12:00:00 -0300')
+      expect(Time.now.year).to eq(2010)
+    end
+
+    it "allows stubbing database calls a mock object" do
+      class Customer
+        attr_accessor :name
+        def self.find
+          # database lookup, returns one object
+        end
+      end
+
+      dbl = double('Mock Customer')
+      allow(dbl).to receive(:name).and_return('Bob')
+
+      allow(Customer).to receive(:find).and_return(dbl)
+
+    	customer = Customer.find
+      expect(customer.name).to eq('Bob')
+    end
+
+    it "allows stubbing database calls with many mock objects" do
+      class Customer
+        attr_accessor :name
+        def self.all
+          # database lookup, returns array of objects
+        end
+      end
+
+      c1 = double('First Customer', :name => 'Bob')
+    	c2 = double('Second Customer', :name => 'Mary')
+
+      allow(Customer).to receive(:all).and_return([c1,c2])
+
+    	customers = Customer.all
+      expect(customers[1].name).to eq('Mary')
+    end
+
+  end
+
+
 end
